ea0dbc46ac18947ade4ad026bdb13627190b7066
Nozomi <13172293+TheNozomi@users.noreply.github.com>
2021-08-06 22:43:00 -0300

    Added inactive members cleanup task and wiki links parser

---
+++ src/events/messageHandlers/WikiLinksParser.ts
@@ -0,0 +1,48 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Event, Events, EventOptions } from '@sapphire/framework';
+
+import type { Message } from 'discord.js';
+
+const InterwikiPrefixes = {
+  dev: 'https://dev.fandom.com/wiki/$1',
+  github: 'https://github.com/$1',
+  gh: 'https://github.com/$1', // github alias
+  google: 'https://google.com/?q=$1',
+  mw: 'https://www.mediawiki.org/wiki/$1',
+  ud: 'https://www.urbandictionary.com/define.php?term=$1',
+  w: 'https://community.fandom.com/wiki/w:$1',
+  c: 'https://community.fandom.com/wiki/$1',
+  wikipedia: 'https://en.wikipedia.org/wiki/$1',
+  wp: 'https://en.wikipedia.org/wiki/$1' // wikipedia alias
+};
+
+@ApplyOptions<EventOptions>({
+  event: Events.Message
+})
+class WikiLinksParser extends Event {
+  public async run(message: Message): Promise<void> {
+    const { client } = this.context;
+
+    if (!message.content) return;
+    const capturedLinks = message.content.match(/\[\[(.*?)(\|(.*?))?\]\](?=(?:[^`]*`[^`]*`)*[^`]*$)/g);
+    if (!capturedLinks || !capturedLinks.length) return;
+    let parsedLinks: string[] = [];
+
+    for (const link of capturedLinks) {
+      // Proceso de parseo
+      const groups = link.match(/\[\[(.*?)(\|(.*?))?\]\]/);
+      if (!groups || groups.length < 2) return;
+      const prefix = groups[1].match(/^(.*?):(.*)/) || [];
+      // desperate times ask for desperate measures
+      const [, prefixCandidate, prefixContent] = prefix as [never, keyof typeof InterwikiPrefixes, string];
+      const interwikiUrl: string = InterwikiPrefixes[prefixCandidate]
+        ? InterwikiPrefixes[prefixCandidate].replace(/\$1/g, prefixContent.replace(/ /g, '_'))
+        : `https://comunidad.fandom.com/wiki/${groups[1].replace(/ /g, '_')}`;
+      parsedLinks = parsedLinks.concat(interwikiUrl);
+    }
+
+    message.reply(parsedLinks.join('\n')).catch(client.logException);
+  }
+}
+
+export default WikiLinksParser;
---
+++ src/tasks/KickNonVerifiedMembers.ts
@@ -0,0 +1,51 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { CronJob } from 'cron';
+
+import { Task } from '#lib/structures/Task';
+import { env } from '#lib/env';
+
+import type { PieceOptions } from '@sapphire/pieces';
+import type { Guild, Role, TextChannel } from 'discord.js';
+
+@ApplyOptions<PieceOptions>({ enabled: true })
+export class KickNonVerifiedMembers extends Task {
+  public async run(): Promise<void> {
+    const { client } = this.context;
+
+    client.logger.info('Running scheduled task: KickNonVerifiedMembers');
+    const guild = client.guilds.resolve(env.GUILD_ID) as Guild,
+      newUserRole = guild.roles.resolve(env.NEWUSER_ROLE) as Role,
+      logsChannel = guild.channels.resolve(env.LOGS_CHANNEL) as TextChannel;
+
+    newUserRole.members.each((member): void => {
+      if (member.roles.cache.has(env.USER_ROLE)) {
+        /*
+        * if member has already verified but still has the newUserRole, remove newUserRole from them.
+        * this normally shouldn't happen, but... ¯\_(ツ)_/¯
+        */
+        member.roles.remove(newUserRole).catch(client.logException);
+        return;
+      }
+      if (!member.joinedTimestamp) return;
+      const timestampDifference = new Date().getTime() - member.joinedTimestamp,
+        differenceDays = timestampDifference / (1000 * 3600 * 24);
+      if (differenceDays >= 2) {
+        member.send(`¡Hola! Esto es una notificación para informarte que se te ha expulsado automáticamente de la **Confederación de Fandom Hispano** porque no has completado a tiempo el formulario. No te preocupes, puedes volver a unirte cuando desees mediante este enlace: <${env.GUILD_INVITE}>, o desde <https://confederacion-hispana.fandom.com/es/>. ¡Te esperamos!`)
+          .catch(client.logException)
+          .finally(() => {
+            member.kick('Expulsión automática pasadas 48 horas sin llenar el formulario').then(() => {
+              client.logger.info(`Kicked ${member.user.username}#${member.user.discriminator}`, { task: 'KickNonVerifiedMembers' });
+              logsChannel.send(`Se expulsó a <@!${member.user.id}> (${member.user.username}#${member.user.discriminator}) al cumplirse 48 horas sin llenar el formulario.`).catch(client.logException);
+            }).catch((err) => {
+              logsChannel.send(`Error al expulsar a <@!${member.user.id}>: ${err.message} (${err.name})`).catch(client.logException);
+              client.logException(err);
+            });
+          });
+      }
+    });
+  }
+
+  public create(): CronJob {
+    return new CronJob('0 * * * *', () => this.run(), null, true);
+  }
+}
