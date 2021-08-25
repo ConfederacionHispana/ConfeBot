50505859abe1b34d3495806e635e8bb890b8c723
bitomic <35199700+bitomic@users.noreply.github.com>
2021-08-25 13:06:05 -0600

    feat: kick accounts created less than 3 days ago

--- src/events/MemberAddEvent.ts
+++ src/events/MemberAddEvent.ts
@@ -8,6 +8,11 @@ import type { GuildMember } from 'discord.js';
 })
 class MemberAddEvent extends Event {
   public run(member: GuildMember): void {
+    this.logNewMember(member);
+    this.checkAccountAge(member);
+  }
+
+  private logNewMember(member: GuildMember): void {
     const { client } = this.context;
 
     client.logger.info('Nuevo miembro', {
@@ -16,6 +21,30 @@ class MemberAddEvent extends Event {
       source: 'user-log'
     });
   }
+
+  private checkAccountAge(member: GuildMember): void {
+    const SECONDS_IN_DAY = 1000 * 60 * 60 * 24;
+    const created = member.user.createdAt.getTime();
+    const now = Date.now();
+    const age = Math.floor((now - created) / SECONDS_IN_DAY);
+    if (age < MemberAddEvent.MINIMUM_DAYS_AGE) {
+      const { client } = this.context;
+      member.kick(`Cuenta creada hace menos de 3 días.`).catch((e) => {
+        client.logException(e, {
+          userId: member.user.id,
+          userTag: member.user.tag,
+          source: 'user-log'
+        });
+      });
+      client.logger.info('Miembro expulsado', {
+        userId: member.user.id,
+        userTag: member.user.tag,
+        source: 'user-log'
+      });
+    }
+  }
+
+  static MINIMUM_DAYS_AGE = 3;
 }
 
 export default MemberAddEvent;
