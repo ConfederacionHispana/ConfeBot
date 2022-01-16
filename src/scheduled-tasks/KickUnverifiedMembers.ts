04b5fb801b23026fdbb602f22144db69b1a5f992
TheNozomi <13172293+TheNozomi@users.noreply.github.com>
2022-01-16 16:16:32 -0300

    refactor(KickUnverifiedMembers): use async/await

--- src/scheduled-tasks/KickUnverifiedMembers.ts
+++ src/scheduled-tasks/KickUnverifiedMembers.ts
@@ -12,7 +12,7 @@ export class IntervalTask extends ScheduledTask {
     });
   }
 
-  public run(): void {
+  public async run(): Promise<void> {
     const { client } = this.container;
 
     client.logger.info('Running scheduled task: KickNonVerifiedMembers');
@@ -20,46 +20,47 @@ export class IntervalTask extends ScheduledTask {
     const newUserRole = guild.roles.resolve(env.NEWUSER_ROLE) as Role;
     const logsChannel = guild.channels.resolve(env.LOGS_CHANNEL) as TextChannel;
 
-    newUserRole.members.each((member): void => {
+    for (const [, member] of newUserRole.members.entries()) {
       if (member.roles.cache.has(env.USER_ROLE)) {
         /*
          * if member has already verified but still has the newUserRole, remove newUserRole from them.
          * this normally shouldn't happen, but... ¯\_(ツ)_/¯
          */
         member.roles.remove(newUserRole).catch(client.logException);
-        return;
+        continue;
       }
-      if (!member.joinedTimestamp) return;
+      if (!member.joinedTimestamp) continue;
+
       const timestampDifference = new Date().getTime() - member.joinedTimestamp;
       const differenceDays = timestampDifference / (1000 * 3600 * 24);
       if (differenceDays >= 2) {
-        member
-          .send(
-            `¡Hola! Esto es una notificación para informarte que se te ha expulsado automáticamente de la **Confederación de Fandom Hispano** porque no has completado a tiempo el formulario. No te preocupes, puedes volver a unirte cuando desees mediante este enlace: <${env.GUILD_INVITE}>, o desde <https://confederacion-hispana.fandom.com/es/>. ¡Te esperamos!`
-          )
-          .catch(client.logException)
-          .finally(() => {
-            member
-              .kick('Expulsión automática pasadas 48 horas sin llenar el formulario')
-              .then(() => {
-                client.logger.info(`Kicked ${member.user.username}#${member.user.discriminator}`, {
-                  task: 'KickNonVerifiedMembers'
-                });
-                logsChannel
-                  .send(
-                    `Se expulsó a <@!${member.user.id}> (${member.user.username}#${member.user.discriminator}) al cumplirse 48 horas sin llenar el formulario.`
-                  )
-                  .catch(client.logException);
-              })
-              .catch((err) => {
-                logsChannel
-                  .send(`Error al expulsar a <@!${member.user.id}>: ${err.message} (${err.name})`)
-                  .catch(client.logException);
-                client.logException(err);
-              });
+        try {
+          await member
+            .send(
+              `¡Hola! Esto es una notificación para informarte que se te ha expulsado automáticamente de la **Confederación de Fandom Hispano** porque no has completado a tiempo el formulario. No te preocupes, puedes volver a unirte cuando desees mediante este enlace: <${env.GUILD_INVITE}>, o desde <https://confederacion-hispana.fandom.com/es/>. ¡Te esperamos!`
+            )
+            .catch(client.logException);
+
+          await member.kick('Expulsión automática pasadas 48 horas sin llenar el formulario');
+          client.logger.info(`Kicked ${member.user.username}#${member.user.discriminator}`, {
+            task: 'KickNonVerifiedMembers'
           });
+
+          await logsChannel
+            .send(
+              `Se expulsó a <@!${member.user.id}> (${member.user.username}#${member.user.discriminator}) al cumplirse 48 horas sin llenar el formulario.`
+            )
+            .catch(client.logException);
+        } catch (err) {
+          if (err instanceof Error) {
+            client.logException(err);
+            logsChannel
+              .send(`Error al expulsar a <@!${member.user.id}>: ${err.message} (${err.name})`)
+              .catch(client.logException);
+          }
+        }
       }
-    });
+    }
   }
 }
 
