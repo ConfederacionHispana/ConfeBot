99b9af1f5ef439f48fdda0e915f4c08810d008bc
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-18 13:40:27 -0600

    feat: slashify user associate

--- src/commands/confe/UserAssociateCommand.ts
+++ src/commands/confe/UserAssociateCommand.ts
@@ -1,43 +1,60 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Args, Command } from '@sapphire/framework';
+import { ApplicationCommandRegistry, Args, Command } from '@sapphire/framework';
 import { env, FandomUtilities, NonExistentUser } from '../../lib';
 
 import type { CommandOptions } from '@sapphire/framework';
-import type { Message, TextChannel } from 'discord.js';
+import type { CommandInteraction, Guild, GuildMember, Message, TextChannel, User } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
-  aliases: ['associate', 'forceverify', 'manualverify'],
-  flags: ['guest']
+  aliases: ['forceverify', 'manualverify'],
+  description: 'Verifica manualmente a un usuario.',
+  flags: ['guest'],
+  name: 'associate',
+  preconditions: ['StaffOnly'],
+  runIn: 'GUILD_ANY'
 })
 export class UserAssociateCommand extends Command {
-  public async messageRun(message: Message, args: Args): Promise<void> {
-    const memberHasRole = message.member?.roles.cache.has(env.STAFF_ROLE);
-    if (!memberHasRole) return;
+  public override registerApplicationCommands(registry: ApplicationCommandRegistry): void {
+    registry.registerChatInputCommand(
+      {
+        description: this.description,
+        name: this.name,
+        options: [
+          {
+            description: 'Miembro del servidor a verificar',
+            name: 'miembro',
+            required: true,
+            type: 'USER'
+          },
+          {
+            description: 'Nombre de usuario en Fandom',
+            name: 'usuario',
+            required: false,
+            type: 'STRING'
+          },
+          {
+            description: 'Verificar como invitado',
+            name: 'invitado',
+            required: false,
+            type: 'BOOLEAN'
+          }
+        ]
+      },
+      this.container.client.chatInputCommandsData.get(this.name)
+    );
+  }
+
+  public async run({ author, guild, member, username, guest }: { author: User, guild: Guild, member: GuildMember, username?: string | null, guest?: boolean | null }): Promise<string> {
+    if (!username && !guest) {
+      return '❓ Se requiere un nombre de usuario de Fandom, o el flag `--guest` para verificar como invitado.';
+    }
 
     const { client } = this.container;
-
-    const targetUserMatch = await args.pickResult('member');
-    if (!targetUserMatch.success) {
-      message.reply('❓ No encontré al usuario que buscas.').catch(client.logException);
-      return;
-    }
-    const targetUser = targetUserMatch.value;
-    const guestFlag = args.getFlags('guest');
-    const fandomUserMatch = await args.restResult('string');
-    if (!fandomUserMatch.success && !guestFlag) {
-      message
-        .reply('❓ Se requiere un nombre de usuario de Fandom, o el flag `--guest` para verificar como invitado.')
-        .catch(client.logException);
-      return;
-    }
-
-    const fandomUser = fandomUserMatch.value;
-
-    if (fandomUser) {
+    if (username) {
       try {
-        const mwUser = await FandomUtilities.getUserInfo('comunidad', fandomUser);
+        const mwUser = await FandomUtilities.getUserInfo('comunidad', username);
 
-        const { id } = targetUser;
+        const { id } = member.user;
         const model = this.container.stores.get('models').get('user');
         const dbUser = await model.findUserBySnowflake(id) ?? model.getDefaultUser(id);
 
@@ -59,33 +76,78 @@ export class UserAssociateCommand extends Command {
         await model.create(dbUser)
           .catch(client.logException);
       } catch (err) {
+        client.logException(err);
         if (err instanceof Error) {
-          message
-            .reply(
-              `❌ ${err instanceof NonExistentUser ? 'La cuenta de usuario especificada no existe.' : err.message}`
-            )
-            .catch(client.logException);
-          return;
+          return `❌ ${err instanceof NonExistentUser ? 'La cuenta de usuario especificada no existe.' : err.message}`;
         }
+        return `❌ Ha ocurrido un error inesperado.`;
       }
     }
-
-    const logsChannel = message.guild?.channels.resolve(env.LOGS_CHANNEL) as TextChannel;
-    const rolesToAdd = fandomUser ? [env.USER_ROLE, env.FDUSER_ROLE] : [env.USER_ROLE];
-    const logReason = `✅ <@!${message.member?.id}> verificó a <@!${targetUser.id}> ${
-      fandomUser ? `con la cuenta de Fandom **${fandomUser}**` : 'como invitado'
+  
+    const logsChannel = await guild.channels.fetch(env.LOGS_CHANNEL) as TextChannel;
+    const rolesToAdd = username ? [env.USER_ROLE, env.FDUSER_ROLE] : [env.USER_ROLE];
+    const logReason = `✅ <@!${author.id}> verificó a <@!${member.user.id}> ${
+      username ? `con la cuenta de Fandom **${username}**` : 'como invitado'
     }`;
 
-    targetUser.roles
-      .add(
+    await Promise.all([
+      member.roles.add(
         rolesToAdd,
-        `Verificado manualmente por ${message.member?.user.username}#${message.member?.user.discriminator}`
-      )
-      .then(() => {
-        targetUser.roles.remove(env.NEWUSER_ROLE).catch(client.logException);
-        logsChannel.send(logReason).catch(client.logException);
-        message.react('✅').catch(client.logException);
-      })
-      .catch(client.logException);
+        `Verificado manualmente por ${author.tag}`
+      ),
+      member.roles.remove(env.NEWUSER_ROLE),
+      logsChannel.send(logReason)
+    ]).catch(client.logException);
+
+    return `Verificación exitosa.`;
+  }
+
+  public async messageRun(message: Message, args: Args): Promise<void> {
+    if (!message.guild) return;
+
+    const { client } = this.container;
+
+    const targetUserMatch = await args.pickResult('member');
+    if (!targetUserMatch.success) {
+      void message.reply('❓ No encontré al usuario que buscas.');
+      return;
+    }
+    const targetUser = targetUserMatch.value;
+    const guestFlag = args.getFlags('guest');
+    const fandomUserMatch = await args.restResult('string');
+
+    const reply = await this.run({
+      author: message.author,
+      guild: message.guild,
+      member: targetUser,
+      guest: guestFlag,
+      username: fandomUserMatch.value
+    });
+    void message.reply(reply);
+  }
+
+  public async chatInputRun(interaction: CommandInteraction<'present'>): Promise<void> {
+    await interaction.deferReply();
+    const user = interaction.options.getUser('miembro', true);
+    const fandomUsername = interaction.options.getString('usuario');
+    const guest = interaction.options.getBoolean('invitado');
+
+    const guild = interaction.guild ?? await this.container.client.guilds.fetch(interaction.guildId);
+    const member = await guild.members.fetch(user.id)
+      .catch(() => null);
+
+    if (!member) {
+      void interaction.editReply(`El usuario no se encuentra en el servidor.`);
+      return;
+    }
+
+    const reply = await this.run({
+      author: interaction.user,
+      guild,
+      member,
+      guest,
+      username: fandomUsername
+    });
+    void interaction.editReply(reply);
   }
 }
