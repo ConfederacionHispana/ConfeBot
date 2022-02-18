4d8d751c112e6a13c25b05309bc74242d3d38476
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-18 00:41:48 -0600

    feat: slashify starboard

--- src/commands/config/StarboardCommand.ts
+++ src/commands/config/StarboardCommand.ts
@@ -1,43 +1,68 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Args, Command, CommandOptions, UserError } from '@sapphire/framework';
-import { Permissions } from 'discord.js';
+import { ApplicationCommandRegistry, Args, Command, CommandOptions } from '@sapphire/framework';
+import { CommandInteraction, Permissions } from 'discord.js';
 
 import type { Message } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
-  aliases: ['starboard']
+  description: 'Configura (o consulta) el canal de pines',
+  name: 'starboard',
+  runIn: 'GUILD_ANY'
 })
 export class StarboardCommand extends Command {
-  public async messageRun(message: Message, args: Args): Promise<void> {
-    const { client } = this.container;
-    if (!message.guildId) {
-      void message.channel.send('Este comando solo puede ejecutarse en un servidor.');
-      return;
-    }
+  public override registerApplicationCommands(registry: ApplicationCommandRegistry): void {
+    registry.registerChatInputCommand(
+      {
+        description: this.description,
+        name: this.name,
+        options: [
+          {
+            channelTypes: ['GUILD_TEXT'],
+            description: 'Configurar canal de pines',
+            name: 'canal',
+            required: false,
+            type: 'CHANNEL'
+          }
+        ]
+      },
+      this.container.client.chatInputCommandsData.get(this.name)
+    );
+  }
+
+  public async run(guildId: string, channelId?: string, permissions?: Permissions): Promise<string> {
     const model = this.container.stores.get('models').get('guild');
-    const oldChannel = await model.getChannel(message.guildId, 'starboard');
+    const oldChannel = await model.getChannel(guildId, 'starboard');
+    if (!channelId) {
+      if (oldChannel) {
+        return `Canal de pines: <#${oldChannel}>`;
+      } 
+      return `No hay un canal de pines configurado.`;
+      
+    } else if (permissions?.has(Permissions.FLAGS.MANAGE_GUILD)) {
+      await model.setChannel(guildId, 'starboard', channelId);
+      return `Se ha configurado el canal de pines: <#${channelId}>.`;
+    } 
+    return 'No tienes permisos para cambiar el canal de pines.';
+    
+  }
 
-    try {
-      const channel = await args.pick('channel').catch(() => null);
-      if (!channel) {
-        if (oldChannel) {
-          void message.channel.send(`Canal de pines: <#${oldChannel}>`);
-        }
-        return;
-      }
+  public async messageRun(message: Message, args: Args): Promise<void> {
+    if (!message.guildId) return;
+    const channel = await args.pick('guildTextChannel')
+      .catch(() => null);
+    
+    const reply = await this.run(message.guildId, channel?.id, message.member?.permissions);
+    void message.reply(reply);
+  }
 
-      if (message.member?.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
-        await model.setChannel(message.guildId, 'starboard', channel.id);
-        void message.channel.send(`Canal de pines: <#${channel.id}>`);
-        return;
-      }
-      void message.reply('No tienes permisos para cambiar el canal de pines.');
-      return;
-    } catch (err) {
-      if (err instanceof Error) {
-        client.logException(err);
-        void message.channel.send('Ocurrió un error al intentar cambiar el canal de pines.');
-      }
-    }
+  public async chatInputRun(interaction: CommandInteraction<'present'>): Promise<void> {
+    await interaction.deferReply();
+    const channel = interaction.options.getChannel('canal');
+    const guild = interaction.guild ?? await this.container.client.guilds.fetch(interaction.guildId);
+    const member = await guild.members.fetch(interaction.user.id)
+      .catch(() => null);
+    
+    const reply = await this.run(interaction.guildId, channel?.id, member?.permissions);
+    void interaction.editReply(reply);
   }
 }
