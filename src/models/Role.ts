d492562c09c475d9ce51dce03b1bc98a0a0956ce
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-19 17:36:07 -0600

    feat(models): role

---
+++ src/models/Role.ts
@@ -0,0 +1,43 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Model } from '../lib';
+import type { PieceOptions } from '@sapphire/pieces';
+
+export interface IRole {
+    creation: Date
+	emoji?: string
+	label?: string
+	message: string
+	role: string
+}
+
+@ApplyOptions<PieceOptions>({
+  name: 'role'
+})
+export class RoleModel extends Model {
+  public async create(document: IRole): Promise<void> {
+    const collection = this.container.mongodb.collection<IRole>('roles');
+    await collection.insertOne(document);
+  }
+
+  public countRolesInMessage(message: string): Promise<number> {
+    const collection = this.container.mongodb.collection<IRole>('roles');
+    return collection.countDocuments({ message });
+  }
+    
+  public getMessageRoles(message: string): Promise<IRole[]> {
+    const collection = this.container.mongodb.collection<IRole>('roles');
+    return collection.find({ message }).sort({ creation: 1 }).toArray();
+  }
+
+  public async isSet(message: string, role: string): Promise<boolean> {
+    const collection = this.container.mongodb.collection<IRole>('roles');
+    const document = await collection.findOne({ message, role });
+    return document ? true : false;
+  }
+}
+
+declare global {
+    interface ModelRegistryEntries {
+        'role': RoleModel
+    }
+}
