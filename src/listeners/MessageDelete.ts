979b608234a590d148b7fb293f216648202c0c1b
TheNozomi <13172293+TheNozomi@users.noreply.github.com>
2022-04-17 01:42:27 -0300

    feat(logs): attempt to detect NQN messages and skip logging them

--- src/listeners/MessageDelete.ts
+++ src/listeners/MessageDelete.ts
@@ -1,6 +1,8 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, type ListenerOptions } from '@sapphire/framework';
 import { MessageEmbed, type BaseGuildTextChannel, type Message } from 'discord.js';
+import { stringSimilarity } from 'string-similarity-js';
+import { sleep } from '../lib';
 
 @ApplyOptions<ListenerOptions>({
   event: Events.MessageDelete,
@@ -10,6 +12,7 @@ export class MessageDeleteListener extends Listener {
     const { client } = this.container;
 
     if (!message.guild) return;
+    if (message.author.bot) return;
 
     const guild = this.container.stores.get('models').get('guild'),
       logsChannelId = await guild.getChannel(message.guild.id, 'logs'),
@@ -20,6 +23,28 @@ export class MessageDeleteListener extends Listener {
     if (!logsChannel) {
       client.logger.info(`No logs channel configured for ${message.guild.name}`);
       return;
+    }
+
+    if (message.content.length) {
+      /**
+       * Try to find a similar message sent after the deleted one, and from a webhook with the same name as the deleted message's author
+       * If found, compare the content
+       * If the content is similar, we can be 99% sure that the message was deleted by NQN, so we don't log it
+       */
+
+      await sleep(2000);
+
+      const webhookMessages = message.channel.messages.cache.filter((m) => m.webhookId !== null && m.createdTimestamp > message.createdTimestamp),
+        webhookMessagesSameUser = webhookMessages.filter((m) => m.author.username === message.member?.displayName);
+
+      const contentMatch = webhookMessagesSameUser.find((m) => {
+        const messageContentWithoutEmojiIDs = m.content.replace(/<(a)?(:[^:]+:)([0-9]+)>/g, '$2'),
+          similarityScore = stringSimilarity(messageContentWithoutEmojiIDs, message.content);
+
+        return similarityScore > 0.99;
+      });
+
+      if (contentMatch) return;
     }
 
     const embed = new MessageEmbed()
