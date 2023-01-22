cbdd5e3961e0252c16a3b626d4392cbf025d57a6
TheNozomi <13172293+TheNozomi@users.noreply.github.com>
2023-01-22 18:19:44 -0300

    feat(CreateInviteCommand): create invites via bot

--- src/preconditions/StaffOnly.ts
+++ src/preconditions/StaffOnly.ts
@@ -1,7 +1,7 @@
 import type { PreconditionOptions, PreconditionResult } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 import { env } from '../lib/env';
-import type { Message } from 'discord.js';
+import type { CommandInteraction, GuildMemberRoleManager, Message } from 'discord.js';
 import { Precondition } from '@sapphire/framework';
 
 @ApplyOptions<PreconditionOptions>({
@@ -19,6 +19,10 @@ export class UserPrecondition extends Precondition {
 
   public messageRun(message: Message): PreconditionResult {
     return this.evaluate(message.author.id === '697553237867364362' || (message.member && message.member.roles.cache.has(env.STAFF_ROLE)));
+  }
+
+  public chatInputRun(interaction: CommandInteraction<'cached' | 'raw'>): PreconditionResult {
+    return this.evaluate(interaction.user.id === '697553237867364362' || (interaction.member && (interaction.member.roles as GuildMemberRoleManager).cache.has(env.STAFF_ROLE)));
   }
 }
 
